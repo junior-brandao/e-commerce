fcc4d0cd6b50ebb915001219d5c80cec430f153a
junior-brandao <juniorbrandaodev@gmail.com>
2023-07-30 16:24:42 -0300

    add order

---
+++ src/main/java/com/eblj/sistema/_e_commerce/entities/Order.java
@@ -0,0 +1,64 @@
+package com.eblj.sistema._e_commerce.entities;
+
+import com.eblj.sistema._e_commerce.enuns.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")//salva no banco com utc
+  private Instant moment;
+  @JsonFormat(shape = JsonFormat.Shape.STRING)
+  private OrderStatus status;
+  @ManyToOne
+  @JoinColumn(name = "client_id")
+  private User client;
+
+  public Order(){}
+
+  public Order(long id, Instant moment, OrderStatus status, User client) {
+    this.id = id;
+    this.moment = moment;
+    this.status = status;
+    this.client = client;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public Instant getMoment() {
+    return moment;
+  }
+
+  public void setMoment(Instant moment) {
+    this.moment = moment;
+  }
+
+  public OrderStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(OrderStatus status) {
+    this.status = status;
+  }
+
+  public User getClient() {
+    return client;
+  }
+
+  public void setClient(User client) {
+    this.client = client;
+  }
+}
---
+++ src/main/java/com/eblj/sistema/_e_commerce/enuns/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.eblj.sistema._e_commerce.enuns;
+
+public enum OrderStatus {
+
+  WAITING_PAYMENT,
+  PAID,
+  SHIPPED,
+  DELIVERED,
+  CANCELD;
+
+}
